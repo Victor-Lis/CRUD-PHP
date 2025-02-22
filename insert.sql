f241b02ac79b8a7c0f76df8027c9cf7b0b8adffc
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2025-02-22 17:54:20 -0300

    fix: Atualizando .gitignore

--- insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `colaboradores` ( `nome` , `sobrenome` , `email` , `formacao` , `descricao` )
-VALUES (
-'Victor', 'Lis Bronzo', 'victor@gmail.com', 'Técnico em DS', 'Profissional...'
-)
